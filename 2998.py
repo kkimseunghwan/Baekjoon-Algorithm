e3d330e77a8d3dbfa7990e1b24a3cc5778d907d9
Congnamul <rhian3493@gmail.com>
2023-02-16 02:31:19 +0900

    0216

--- 2998.py
+++ 2998.py
@@ -1,22 +1,19 @@
 def Make8Number(number):
-  strNum = str(number)
   returnNum = ""
-  while True:
-    if len(strNum) % 3 != 0:
-      strNum = "0" + strNum
-    else: break
+  while len(number) % 3 != 0:
+      number = "0" + number
   
-  for i in range(0, len(strNum), 3):
+  for i in range(0, len(number), 3):
     plus = 0
-    if int(strNum[i]) == 1:
+    if int(number[i]) == 1:
       plus += 4
-    if int(strNum[i+1]) == 1:
+    if int(number[i+1]) == 1:
       plus += 2
-    if int(strNum[i+2]) == 1:
+    if int(number[i+2]) == 1:
       plus += 1
     returnNum += str(plus)
 
   return int(returnNum)
 
-num = int(input())
+num = input()
 print(Make8Number(num))
