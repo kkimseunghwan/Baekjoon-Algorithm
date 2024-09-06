5958e907ed3843fb973a196dfb4ec9f55686d093
김승환 <82988117+kkimseunghwan@users.noreply.github.com>
2024-09-06 14:36:41 +0900

    0906 알고리즘 문제 풀이
    날먹
    솔직히 양심 찔림
    최소 힙 코드짜고 부등호 방향만 바꿔서 구현.
    구조 자체는 동일하니 개졈 정리나 자세히 해놔야겠따

---
+++ 11279 최대힙.py
@@ -0,0 +1,65 @@
+#11279 최대힙
+from sys import stdin
+
+def min_Heap_Append(heap, item, heapLen):
+  i = heapLen
+  heap.append(item) # 힙 크기를 하나 증가하고 마지막 노드에 x를 넣는다.
+
+  while i > 1:
+    if heap[i] > heap[i//2]:
+      heap[i], heap[i//2] = heap[i//2], heap[i]
+      i = i//2
+    else:
+      break
+
+
+def min_Heap_Pop(heap, heapLen):
+  item = heap[1] # 최대값 저장
+  heap[1] = heap[heapLen] # 마지막 노드를 루트 노드로 이동
+  heap.pop() # 마지막 노드 삭제
+  heapLen -= 1 # 힙 크기 하나 감소
+
+  i = 1 # 루트 노드 검사 시작.
+  while i*2 <= heapLen:
+    left = i*2
+    right = i*2+1
+
+    if right <= heapLen: # 노드가 오른쪽에 존재하는지 확인. (2개 남음 대비)
+      if heap[i] > heap[i*2] and heap[i] > heap[i*2+1]:
+        break
+      elif heap[i*2] > heap[i*2+1]:
+        heap[i], heap[i*2] = heap[i*2], heap[i]
+        i = i*2
+      else:
+        heap[i], heap[i*2+1] = heap[i*2+1], heap[i]
+        i = i*2+1
+    else:
+      if heap[i] < heap[i*2]:
+        heap[i], heap[i*2] = heap[i*2], heap[i]
+      break
+
+  return item
+
+
+N = int(stdin.readline()) # N(1 ≤ N ≤ 100,000)
+#N = int(input())
+heap = [ 0 ]
+heapLen = 0
+for _ in range(N): 
+  x = int(stdin.readline())
+  #x = int(input())
+  if x != 0:
+    heapLen += 1
+    min_Heap_Append(heap, x, heapLen)
+  else:
+    if heapLen < 1:
+      print(0)
+    else:
+      item = min_Heap_Pop(heap, heapLen)
+      heapLen -= 1
+      print(item)
+    
+
+
+
+
